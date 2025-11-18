3d96fa2de130df5e74dc8e5423501b569d048d64
Lebon24 <92101264+Lebon24@users.noreply.github.com>
2025-11-18 00:32:02 +0000

    Require SSL for database connection
    Updated DATABASES configuration to require SSL.

--- config/settings.py
+++ config/settings.py
@@ -101,8 +101,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+DATABASES = {
+  'default':{
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / 'db.sqlite3',
+  }
+}
+
 if 'DATABASE_URL' in os.environ:
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=False)
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
