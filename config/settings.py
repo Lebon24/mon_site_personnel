d55c2e8a9104aeec4efeda4fa4276c05f9e65481
Lebon24 <92101264+Lebon24@users.noreply.github.com>
2025-11-19 12:17:38 +0000

    Refactor database configuration for Django settings

--- config/settings.py
+++ config/settings.py
@@ -102,14 +102,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-  'default':{
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': BASE_DIR / 'db.sqlite3',
-  }
+  'default': dj_dataase_url.config(
+    default='sqlite:///db.sqlite3',
+    conn_max_age=600,
+    ssl_require=False
+  )
 }
-
-if 'DATABASE_URL' in os.environ:
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=False)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
